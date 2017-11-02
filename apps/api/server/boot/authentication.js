4736717616daaf657d40568637d8b4a5dd3f2e39
Bram Borggreve <borggreve@gmail.com>
2017-11-02 14:33:00 +0700

    feat(api): add support for currentUserLiteral 'me'

--- apps/api/server/boot/authentication.js
+++ apps/api/server/boot/authentication.js
@@ -1,6 +1,12 @@
 'use strict'
+const loopback = require('loopback')
 
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth()
+
+  server.middleware('auth', loopback.token({
+    model: server.models.SystemAccessToken,
+    currentUserLiteral: 'me'
+  }));
 }
