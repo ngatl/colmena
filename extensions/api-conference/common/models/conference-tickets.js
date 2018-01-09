ef7381179cd49bd27b1a8d9c7e255fec2cbec2cb
Bram Borggreve <borggreve@gmail.com>
2018-01-10 02:27:50 +0700

    Add Attendee, AttendeeNotes models

--- extensions/api-conference/common/models/conference-tickets.js
+++ extensions/api-conference/common/models/conference-tickets.js
@@ -44,4 +44,63 @@ module.exports = function(ConferenceTicket) {
     http: { path: '/sync', verb: 'get' },
   })
 
+  const findOrCreate = attendee => {
+    const { ConferenceAttendee } = ConferenceTicket.app.models;
+
+    return ConferenceAttendee
+      .findOne({where: {email: attendee.email}})
+      .then(res => res ? res : ConferenceAttendee.create(attendee))
+  }
+
+  const normalizeAttendee = att => ({
+    company: att['company-name'],
+    email: att.email,
+    name: att.name,
+    phone: att['phone-number'],
+  })
+
+  ConferenceTicket.prototype.extract = function() {
+    return Promise.resolve(findOrCreate(normalizeAttendee(this)))
+      .then(attendee => {
+        this.updateAttribute('conferenceAttendeeId', attendee.id)
+        return attendee
+      })
+  }
+
+  ConferenceTicket.remoteMethod('prototype.extract', {
+    returns: { arg: 'result', type: 'object', root: true },
+    http: { path: '/extract', verb: 'get' },
+  })
+
+
+  ConferenceTicket.extractAll = () => ConferenceTicket
+    .find()
+    .then(items => items
+      .filter(item => item.email)
+      .map(item => item.extract())
+    )
+
+  ConferenceTicket.remoteMethod('extractAll', {
+    returns: { arg: 'result', type: 'object', root: true },
+    http: { path: '/extractAll', verb: 'get' },
+  })
+
+  ConferenceTicket.prototype.claim = function(confirm) {
+    const { ConferenceAttendee } = ConferenceTicket.app.models;
+    return ConferenceAttendee.findById(this.conferenceAttendeeId)
+      .then(attendee => {
+        if (!confirm || this.claimed ) {
+          return Promise.resolve({ claimed: this.claimed || false, confirm, attendee, newClaim: false })
+        }
+        return this.updateAttribute('claimed', new Date())
+          .then(() => Promise.resolve({ claimed: this.claimed, confirm, attendee, newClaim: true }))
+      })
+  }
+
+  ConferenceTicket.remoteMethod('prototype.claim', {
+    accepts: { arg: 'confirm', type: 'boolean', required: false },
+    returns: { arg: 'result', type: 'object', root: true },
+    http: { path: '/claim', verb: 'get' },
+  })
+
 };
