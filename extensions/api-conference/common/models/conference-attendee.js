677bb3aa54db662b2f38fd5fb062480ba0277ead
Bram Borggreve <borggreve@gmail.com>
2018-01-21 11:57:55 +0700

    Make sure username is persisted

--- extensions/api-conference/common/models/conference-attendee.js
+++ extensions/api-conference/common/models/conference-attendee.js
@@ -4,4 +4,10 @@ module.exports = function(ConferenceAttendee) {
 
   ConferenceAttendee.isSponsor = item => item.tags ? (item.tags.indexOf('sponsor') > -1) : false
 
+  ConferenceAttendee.observe('before save', (ctx, next) => {
+    if (ctx.instance && !ctx.instance.username && ctx.instance.email) {
+      ctx.instance.username = ctx.instance.email
+    }
+    next()
+  })
 };
