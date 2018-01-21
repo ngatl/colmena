3f0818320fab6ed82b42be7dc78ccbcf7929a2be
Bram Borggreve <borggreve@gmail.com>
2018-01-22 03:12:49 +0700

    Add sponsorNotes method to Attendee

--- extensions/api-conference/common/models/conference-attendee.js
+++ extensions/api-conference/common/models/conference-attendee.js
@@ -10,4 +10,36 @@ module.exports = function(ConferenceAttendee) {
     }
     next()
   })
+
+  ConferenceAttendee.prototype.sponsorNotes = function() {
+    const notesArray = []
+    const condition = {
+      include: {
+        relation: 'notes',
+        scope: {
+          include: [
+            { relation: 'peer' },
+            { relation: 'attendee' },
+          ]
+        }
+      },
+      where: {
+        sponsor: this.sponsor
+      }
+    }
+
+    const extractNotes = (notes = []) => notes.forEach(note => notesArray.push(note))
+
+    const getNotes = () => ConferenceAttendee.find(condition)
+      .then(attendees => attendees.forEach(attendee => extractNotes(attendee.notes())))
+      .then(() => notesArray)
+
+    return this.sponsor ? getNotes() : Promise.reject(new Error('Attendee is not in a Sponsor Group'))
+  }
+
+  ConferenceAttendee.remoteMethod('prototype.sponsorNotes', {
+    returns: { arg: 'result', type: 'object', root: true },
+    http: { path: '/sponsorNotes', verb: 'get' },
+  })
+
 };
